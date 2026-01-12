560909699a780a2f0ef6f34ead1f6bb188ccb201
Hugh Earp <hugh.earp@gmail.com>
2026-01-12 12:30:45 +0100

    adding undo breakpoint before changing last spelling adjustment

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -62,7 +62,8 @@ vim.b.minisurround_config = {
 
 vim.keymap.set('n', 'ss', 'sairs', { buffer = 0, desc = 'strikeout current line', remap = true })
 
-local action = '<BS><BS><Esc>[s1z=gi<Right>'
+-- set keymap to fix last spelling mistake. And insert an undo breakpoint right before changing spelling
+local action = '<BS><BS><c-g>u<Esc>[s1z=gi<Right>'
 require('mini.keymap').map_combo('i', 'kk', action)
 
 vim.cmd [[
