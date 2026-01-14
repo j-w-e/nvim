f4491b77c29b09baae94b9aa9fc12866c760a6cb
Hugh Earp <hugh.earp@gmail.com>
2026-01-14 19:24:42 +0100

    [blink] removing preset from blink keymap, to make sure it doesn't conflict with tab in mini.snippets

--- plugin/25_notes.lua
+++ plugin/25_notes.lua
@@ -9,14 +9,33 @@ now(function()
     checkout = 'b19413d214068f316c78978b08264ed1c41830ec',
   })
   require('blink.cmp').setup({
+    keymap = {
+      preset = 'none',
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-e>'] = { 'hide', 'fallback' },
+      ['<C-y>'] = { 'select_and_accept', 'fallback' },
+
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+      -- ['<Tab>'] = { 'snippet_forward', 'fallback' },
+      -- ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+      ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+    },
     sources = {
       providers = {
         buffer = {
           score_offset = -100,
           min_keyword_length = 4,
-        }
-      }
-    }
+        },
+      },
+    },
   })
 end)
 
@@ -110,4 +129,3 @@ now(function()
     end,
   })
 end)
-
