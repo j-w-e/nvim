34be00cab7be15d4d83454f6f3933b8b8cbfaa3e
Hugh Earp <hugh.earp@gmail.com>
2026-01-18 14:42:43 +0100

    adding autolist

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -70,3 +70,14 @@ vim.cmd([[
 au BufEnter * syn region markdownLink matchgroup=markdownLinkDelimiter start="(" end=")\ze\_W" keepend contained conceal contains=markdownUrl concealends
 au BufEnter * hi link tkLink markdownLinkText
 ]])
+
+vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>')
+vim.keymap.set('n', 'o', 'o<cmd>AutolistNewBullet<cr>')
+vim.keymap.set('n', 'O', 'O<cmd>AutolistNewBulletBefore<cr>')
+vim.keymap.set('n', '<C-r>', '<cmd>AutolistRecalculate<cr>')
+
+-- functions to recalculate list on edit
+vim.keymap.set('n', '>>', '>><cmd>AutolistRecalculate<cr>')
+vim.keymap.set('n', '<<', '<<<cmd>AutolistRecalculate<cr>')
+vim.keymap.set('n', 'dd', 'dd<cmd>AutolistRecalculate<cr>')
+vim.keymap.set('v', 'd', 'd<cmd>AutolistRecalculate<cr>')
