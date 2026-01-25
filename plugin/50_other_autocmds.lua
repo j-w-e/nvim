f0a39344fea8e40b5d78c56c33ea431bbffe38cc
Hugh Earp <hugh.earp@gmail.com>
2026-01-25 09:55:58 +0100

    formatting changes

--- plugin/50_other_autocmds.lua
+++ plugin/50_other_autocmds.lua
@@ -3,20 +3,20 @@
 local function set_terminal_keymaps()
   local opts = { buffer = 0 }
   -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
-_G.Config.new_autocmd("TermOpen", "*", function(_)
-  vim.cmd.setlocal("nonumber")
+_G.Config.new_autocmd('TermOpen', '*', function(_)
+  vim.cmd.setlocal('nonumber')
   set_terminal_keymaps()
-end, "Set terminal keymaps")
+end, 'Set terminal keymaps')
 
 -- Automatically trigger a reload / re-check of file status if it's changed on disk.
-_G.Config.new_autocmd({ "FocusGained", "BufEnter" }, "*", function()
+_G.Config.new_autocmd({ 'FocusGained', 'BufEnter' }, '*', function()
   vim.cmd.checktime()
-end, "Update from disk")
+end, 'Update from disk')
 
 -- Automatically format markdown files with nested bullet markers
 -- 0 indent  -> "-"
@@ -27,11 +27,11 @@ local function replace_markdown_bullets()
   local bufnr = vim.api.nvim_get_current_buf()
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
-  local bullets = { "-", "*", "+" }
+  local bullets = { '-', '*', '+' }
   local indent_width = vim.bo[bufnr].shiftwidth
 
   -- capture: indentation, bullet, space(s), content
-  local pattern = "^(%s*)([%*%-%+])(%s+)(.*)$"
+  local pattern = '^(%s*)([%*%-%+])(%s+)(.*)$'
 
   for i, line in ipairs(lines) do
     local indent, _, space, content = line:match(pattern)
@@ -50,4 +50,3 @@ end
 _G.Config.new_autocmd('BufWritePre', '*.md', function(o)
   replace_markdown_bullets()
 end, 'Update markdown bullets')
-
