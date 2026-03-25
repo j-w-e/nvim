743bace6124da8e766f879f7dfa7513f1b92147a
Hugh Earp <hugh.earp@gmail.com>
2026-03-25 08:29:55 +0100

    removing extra <c-?> buffer switch keymaps

--- plugin/30_keymaps.lua
+++ plugin/30_keymaps.lua
@@ -9,10 +9,6 @@ end
 nmap('[p', '<Cmd>exe "put! " . v:register<CR>', 'Paste Above')
 nmap(']p', '<Cmd>exe "put "  . v:register<CR>', 'Paste Below')
 
-nmap('<c-n>', '<cmd>bnext<cr>', 'Next buffer')
-nmap('<c-p>', '<cmd>bprev<cr>', 'Prev buffer')
-nmap('<c-x>', '<cmd>lua MiniBufremove.delete()<cr>', 'Delete buffer')
-
 -- Flash keymaps
 vim.keymap.set({ 'n', 'x', 'o' }, '-', function()
   require('flash').jump()
